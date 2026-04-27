54ffcfc99a69527638c46284998ef72c12671191
Luis Henrique <luishpo@al.insper.edu.br>
2026-04-27 17:25:08 -0300

    Exercicio 2 finalizado

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,8 @@ def rolar_dados(numero):
         dados.append(valor)
     return dados
 
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado = dados_rolados[dado_para_guardar]
+    dados_no_estoque.append(dado)
+    dados_rolados.pop(dado_para_guardar)
+    return [dados_rolados, dados_no_estoque]
