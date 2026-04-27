4a6943476bfe8d6e7c2c31da0aab60cb033bffd6
Guilherme <guilhermewh@al.insper.edu.br>
2026-04-27 16:59:21 -0300

    exercicio 1 finalizado

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,9 @@
-print('a')
+import random
+
+def rolar_dados(numero):
+    dados = []
+    for i in range(numero):
+        valor = random.randint(1, 6)
+        dados.append(valor)
+    return dados
+
